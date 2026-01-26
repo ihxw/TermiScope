bf14d8a087fabaaa5868dbb2730c34ca0a6ab165
ihxw2025 <ihxw2025@gmail.com>
2026-01-26 16:50:27 +0800

    1

--- mobile/ionic/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mobile/ionic2',
-  webDir: 'www'
-};
-
-export default config;
